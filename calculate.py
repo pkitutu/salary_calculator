cdfe67871863b886ba34ac0f558bc7eda1ff0510
Kitutu <pkitutu@gmail.com>
2017-08-22 20:22:07 +0300

    calculate script for implementation and input accept

---
+++ calculate.py
@@ -0,0 +1,15 @@
+from salary_calculator import SalaryCalculator
+import sys
+
+try:
+   input_val = sys.argv[1].replace(',','')
+   gross_salary = float(input_val)
+   calc = SalaryCalculator(gross_salary)
+   tax = '{:,}'.format(calc.get_tax())
+   nssf_amount = '{:,}'.format(calc.get_nssfamount())
+   netsalary ='{:,}'.format(calc.get_netsalary())
+   
+   print "PAYE tax: %s NSSF Contribution: %s Net Salary: %s" %(tax, nssf_amount, netsalary)
+except ValueError:
+    print "Not a number"
+
